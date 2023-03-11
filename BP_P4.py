02c616c2f6c1883ceec9ec9e94163cdfa4e3fc51
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-11 22:28:16 +0530

    Add files via upload

---
+++ BP_P4.py
@@ -0,0 +1,11 @@
+# Baisc Python Program
+
+# Area  of Rectangle
+
+l = float(input("Enter length of Rectangle: "))
+w = float(input("Enter width of Rectangle: "))
+
+area = l * w    # formula of area of Rectangle
+
+print("Area of Rectangle is: ", area)
+
